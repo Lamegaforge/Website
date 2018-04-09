8b0f575353888631da7c5bb12c46de7dc225ddca
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-04-09 13:01:48 -0700

    Création des models Category, Post, Thumbnail et User

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    } 
+
+    public function thumbnail()
+    {
+        return $this->belongsTo(Thumbnail::class);
+    }
+}
